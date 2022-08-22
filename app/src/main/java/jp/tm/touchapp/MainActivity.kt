fc3ad093d8c07cb43792aaac50e071f8c15c60c5
focta <focke.wulf7234@gmail.com>
2022-08-23 06:58:41 +0900

    first commit

---
+++ app/src/main/java/jp/tm/touchapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package jp.tm.touchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("Debug", "onCreate()")
+        val ut:TextView = findViewById(R.id.uranaiText)
+        val ub:Button = findViewById(R.id.uranaiBtn)
+
+        // クリック処理
+        ub.setOnClickListener {
+            ut.text = "大吉"
+            Log.d("Debug", "お試し")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Debug", "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Debug", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Debug", "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Debug", "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Debug", "onDestroy()")
+
+    }
+}
